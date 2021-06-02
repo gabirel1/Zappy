54701505f865184a07427d2fabb4d066deffab52
Rafik <rafikmerzouk7@gmail.com>
2021-06-02 17:09:42 +0200

    adding: the info structures

---
+++ include/server/server.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-410-MPL-4-1-zappy-merzouk.rafik
+** File description:
+** server
+*/
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include "def.h"
+
+typedef struct game_info_s
+{
+
+} game_info_t;
+
+typedef struct game_info_s
+{
+    int port;
+} server_info_t;
+
+#endif /* !SERVER_H_ */
