9f957767c02ed6f8a9257a8fd017b638ea1ecc82
Rafik <rafikmerzouk7@gmail.com>
2021-06-21 17:15:55 +0200

    Free memory on look

--- src/server/commands/look.c
+++ src/server/commands/look.c
@@ -9,7 +9,7 @@
 
 tile_t *look_north(game_board_t *game, player_t *player)
 {
-    tile_t *ret = my_malloc(sizeof(tile_t) * ((player->level + 1) * \
+    tile_t *ret = malloc(sizeof(tile_t) * ((player->level + 1) * \
     (player->level + 1)));
     int posx = 0;
     int posy = 0;
@@ -33,7 +33,7 @@ tile_t *look_north(game_board_t *game, player_t *player)
 
 tile_t *look_south(game_board_t *game, player_t *player)
 {
-    tile_t *ret = my_malloc(sizeof(tile_t) * ((player->level + 1) * \
+    tile_t *ret = malloc(sizeof(tile_t) * ((player->level + 1) * \
     (player->level + 1)));
     int posx = 0;
     int posy = 0;
@@ -58,7 +58,7 @@ tile_t *look_south(game_board_t *game, player_t *player)
 
 tile_t *look_east(game_board_t *game, player_t *player)
 {
-    tile_t *ret = my_malloc(sizeof(tile_t) * ((player->level + 1) * \
+    tile_t *ret = malloc(sizeof(tile_t) * ((player->level + 1) * \
     (player->level + 1)));
     int posx = 0;
     int posy = 0;
@@ -83,7 +83,7 @@ tile_t *look_east(game_board_t *game, player_t *player)
 
 tile_t *look_west(game_board_t *game, player_t *player)
 {
-    tile_t *ret = my_malloc(sizeof(tile_t) * ((player->level + 1) * \
+    tile_t *ret = malloc(sizeof(tile_t) * ((player->level + 1) * \
     (player->level + 1)));
     int posx = 0;
     int posy = 0;
@@ -122,6 +122,7 @@ char *look(game_board_t *game, player_t *player)
     if (player->orientation == WEST)
         tiles = look_west(game, player);
     ret = look_tiles(tiles, length, ret, ressources);
+    free(tiles);
     if (ret[strlen(ret) - 1] != ' ')
         ret = my_strcat(ret, " ");
     ret = my_strcat(ret, "]");
